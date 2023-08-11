020488dc029e114b08007e744645573e84c59e2d
Centowsky <centowsky@gmail.com>
2023-08-11 22:23:06 +0200

    Dodana nawigacja

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 import "./styles/App.scss";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about-me" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
